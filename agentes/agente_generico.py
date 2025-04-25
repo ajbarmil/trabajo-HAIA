432980123cb61120a68e00de528fa581484e497e
nacho <nachoturrob@proton.me>
2025-04-25 12:58:26 +0200

    Prueba redes

--- agentes/agente_generico.py
+++ agentes/agente_generico.py
@@ -1,8 +1,30 @@
 import random
+import numpy as np
+
+TAMAÑO = (42, 20, 7)
+
+
+class Red:
+    def __init__(self):
+        self.pesos = [2 * np.random.random((c1, c2)) - 1 for c1, c2 in zip(TAMAÑO[:-1], TAMAÑO[1:])]
+        self.sesgos = [2 * np.random.random(c1) - 1 for c1 in TAMAÑO[1:]]
+
+    def predict(self, x):
+        result = x
+        for i, (peso, sesgo) in enumerate(zip(self.pesos, self.sesgos)):
+            result = result @ peso + sesgo
+
+            if i != len(self.pesos) - 1:
+                result = np.maximum(result, 0)
+            else:
+                exps = np.exp(result)
+                result = exps / exps.sum()
+        return result
+
 
 class Agente:
     def __init__(self):
-        vectores = {}  # por iniciar, índices formato "numagenteXnumenemigo"
+        self.capas = [np.random.random((c1, c2)) for c1, c2 in zip(TAMAÑO[:-1], TAMAÑO[1:])]
 
     def hacer_jugada(self, num_agente, num_enemigo):
         vector = self.vectores[f"{num_agente}X{num_enemigo}"]
